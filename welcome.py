e31667c3d9f824880a7bd0368c0986b7a662acd0
Neha . <neha1221@in.ibm.com>
2017-05-02 05:00:28 -0500

    crazy

--- welcome.py
+++ welcome.py
@@ -1,25 +1,6 @@
-# Copyright 2015 IBM Corp. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 from flask import Flask, render_template, request, jsonify, url_for, send_from_directory
 
-<<<<<<< master
 import json,os,pymongo,ssl
-=======
-import json, os, pymongo, ssl, pandas as pd
->>>>>>> 412a979 yes finallyyy
 from watson_developer_cloud import DiscoveryV1
 from werkzeug.utils import secure_filename
 import base64, pandas as pd
@@ -152,20 +133,10 @@ def upload_document():
             print(instruction_id)
             print(cosine_score)
             df = pd.DataFrame(comb)
-<<<<<<< master
             print(df)
             render_template('result.html', tables=[df.to_html(classes='table', index=False)])
-=======
-            print df
-    return render_template("result.html", tables=[df.to_html(classes='table', index=False)])
->>>>>>> 412a979 yes finallyyy
 
-<<<<<<< master
     return 'file upload successful'
-=======
-
-
->>>>>>> 412a979 yes finallyyy
 
 @app.route("/getmetaData", methods=['GET'])
 def get_document():
